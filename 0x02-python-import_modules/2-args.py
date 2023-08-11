4d6bb75e5db191a2d1e9f1af5916f794b82b612d
favouradagher <favouradagher@gmail.com>
2023-08-11 08:38:12 -0700

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-def main():
-    # Get the number of arguments
-    num_arguments = len(sys.argv) - 1  # The first argument is the script name itself
-
-    # Print the number of arguments
-    if num_arguments == 0:
-        print("0 arguments.")
-    elif num_arguments == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(num_arguments))
-
-    # Print the list of arguments with their positions
-    for index, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(index, arg))
-
 if __name__ == "__main__":
-    main()
-
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} argument".format(len(sys.argv) - 1), end="")
+        if len(sys.argv) > 2:
+            print("s", end="")
+        print(":")
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
