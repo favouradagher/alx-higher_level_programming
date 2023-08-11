391afc017f3a1ef3a828aa257b79ef9db6088f41
favouradagher <favouradagher@gmail.com>
2023-08-11 08:46:03 -0700

    hot fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-
-import sys
-
-def main():
-    # Get all arguments as integers and sum them up
-    total_sum = sum(int(arg) for arg in sys.argv[1:])
-
-    # Print the total sum
-    print(total_sum)
-
 if __name__ == "__main__":
-    main()
-
+    import sys
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum = sum + int(sys.argv[i])
+    print(sum)
