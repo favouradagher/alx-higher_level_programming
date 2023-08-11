8f016eb0582858dfbbd7d820a2cbd7bf8f1390b1
favouradagher <favouradagher@gmail.com>
2023-08-11 06:32:45 -0700

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,20 +1,7 @@
-# 0-add.py
 #!/usr/bin/python3
-
-# Import the add function from add_0.py
 from add_0 import add
 
-def main():
-    # Assign values to variables a and b
+if __name__ == "__main__":
     a = 1
     b = 2
-
-    # Call the add function and store the result
-    result = add(a, b)
-
-    # Print the formatted output
-    print("{} + {} = {}".format(a, b, result))
-
-if __name__ == "__main__":
-    main()
-
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
