31f90440c50e1c50a3fcd4cb7557ba0370caeb3e
favouradagher <favouradagher@gmail.com>
2023-08-11 08:48:09 -0700

    hot fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-
-import sys
-from calculator_1 import add, sub, mul, div
-
-def main():
-    # Check the number of arguments
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    # Extract arguments
+        exit(1)
     a = int(sys.argv[1])
-    operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    # Perform calculations based on the operator
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    # Print the result
-    print("{} {} {} = {}".format(a, operator, b, result))
-
-if __name__ == "__main__":
-    main()
-
+        exit(1)
