33621cda2f45d0eb3f1bdd4eb77b0538f40ff6a3
favour adagher <favouradagher@gmail.com>
2023-08-28 08:58:59 -0700

    hot fix

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        printed = 0
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                printed += 1
+        print()
+        return printed
+    except IndexError:
+        print()
+        return printed
+
